fd8d108bf4330b60f25d08d3e57687c06e0c18fb
Sam Grounds <62066082+Sam-Grounds@users.noreply.github.com>
2020-06-16 12:44:29 +0100

    Uploaded first draft of functions

---
+++ physics.py
@@ -0,0 +1,152 @@
+import math
+#----------------------- Glossary -----------------------||
+
+'''
+force = F
+acceleration = a
+energy = e
+mass = m
+speed of light = c
+'''
+
+
+#----------------------- Constants -----------------------||
+c = 2.99792458 * (10 ** 8) # in m/s
+g = 9.81 # meters per second
+
+if __name__ = '__main__':
+	print('The speed of light:',c,'m/s')
+	print('Gravity: ',g,'m/s')
+
+#----------------------- Forces and Motion -----------------------||
+
+# Einstein's Thoery of Relativity
+def energy(m):
+	# energy = mass * speed of light squared
+	global c
+	# e = m * math.pow(c,2)
+	# e = m * (c ** 2)
+	e = m * (c * c)
+	return e
+
+def acceleration(v,u,t)
+	# acceleration = change in velocity ÷ time
+	a = (v-u) / t
+	return a
+
+# Newton's Second Law of Motion
+def force(m,a):
+	F = m * a
+	return F
+
+def speed(d,t):
+	# speed = distance ÷ time.
+	s = d / t
+	return s
+
+def weight(m):
+	# weight = mass x gravity
+	global g
+	w = m x g
+	return w
+
+     
+def density(m,v):
+	# density = mass ÷ volume 
+	d = m / v
+	return d
+
+
+'''
+p = m x v.     momentum = mass x velocity.
+(mv - mu) = F x t.     change in momentum = Force x time.
+p = F ÷ a.     pressure = force ÷ area.    
+m = F x d.     moment = force x perpendicular distance from pivot.
+'''
+
+
+#----------------------- Electro-Magnetism -----------------------||
+
+'''
+Maxwell's Equations
+Gauss's law: Electric charges produce an electric field. ...
+Gauss's law for magnetism: There are no magnetic monopoles. ...
+Faraday's law: Time-varying magnetic fields produce an electric field.
+Ampère's law: Steady currents and time-varying electric fields (the latter due to Maxwell's correction) produce a magnetic field.
+
+
+Electricity
+
+P = V x I.    power = voltage x current
+V = I x R.    voltage = current x resistance
+Q = I x t.     charge = current x time
+E = V x Q.     energy = voltage x charge
+E = V x I x t.     energy = voltage x current x time
+
+Transformer Equation
+Total cost  =  number of units x cost per unit.
+
+'''
+
+#----------------------- Energy -----------------------||
+'''
+Efficiency (%)  =  (useful energy out ÷ total energy in) x 100.
+GPE = mgh.      GPE = mass x gravity x height.      
+KE  =  ½mv2.     Kinetic Energy = 0·5 x mass x velocity2.
+W = F x d.      work done = force x distance.   
+W = E.      work done = energy transferred.
+P = E ÷ t.      power = energy ÷ time.            
+E = c x m x θ.   energy = specific heat capacity x mass x change in temperature.
+'''
+
+
+
+
+
+#----------------------- Waves -----------------------||
+# v = f x λ.     velocity  =  frequency  x  wavelength
+
+
+
+
+
+
+'''
+engineering forces:
+	friction
+ 	leverage
+'''
+
+# l (string theory)
+
+'''
+Gravity
+Electro-Magnetism
+	Electricity and magnetism
+Weak Nuclear
+	Responsible for the radioactive decay of subatomic particles
+Strong Nuclear
+	Binds the component particles of an atom's nucleus
+
+maxwell's equations - the equations of light
+
+antimatter
+	positron (anti-electron) has a positive charge
+	CERN has detected anti-helium 
+	when antimatter and matter combine, they create a huge force
+
+
+
+'''
+
+'''
+particle zoo - the standard model
+
+
+string theory is the bit below this
+string field theory - michio kaku
+sparticles: super particles
+
+
+
+'''
