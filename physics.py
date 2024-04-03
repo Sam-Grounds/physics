6ef8b378393918e612c720de6bbcc6ba7e8a993e
Sam Grounds <sam.grounds@musician.org>
2024-04-03 17:27:53 +0100

    Update physics.py

--- physics.py
+++ physics.py
@@ -1,152 +1,142 @@
-import math
-#----------------------- Glossary -----------------------||
+class Physics():
+	def __init__(self):
+		#----------------------- Constants -----------------------||
+		#speed of light
+		self.c = 2.99792458 * (10 ** 8)# metres per second
+		# Earth's gravity
+		self.g = 9.81# metres per second
+		return
+	#----------------------- Forces and Motion -----------------------||
+	# Einstein's Theory of Relativity
+	def energy(m):
+		# energy = mass * speed of light squared
+		# e = m * math.pow(c,2)
+		e = m * (self.c ** 2)
+		return e
 
-'''
-force = F
-acceleration = a
-energy = e
-mass = m
-speed of light = c
-'''
+	def acceleration(velocity, velocity2, time):
+		# acceleration = change in velocity ÷ time
+		# a = (v-u) / t
+		a = (velocity - velocity2) / time
+		return a
+
+	# Newton's Second Law of Motion
+	def force(m,a):
+		F = m * a
+		return F
+
+	def speed(distance,time):
+		# speed = distance ÷ time.
+		s = distance / time
+		return s
+
+	def weight(self, mass):
+		w = mass * self.gravity
+		return w
+	     
+	def density(mass,volume):
+		d = mass / volume
+		return d
+
+	def momentum(mass, velocity):
+		p = mass * velocity
+		return p
+
+	def pressure(force,area):
+		# pressure = force ÷ area
+		p = force / area
+		return p
+
+	'''
+	(mv - mu) = F x t.     change in momentum = Force x time.
+	m = F x d.     moment = force x perpendicular distance from pivot
+	'''
+	#----------------------- Electro-Magnetism -----------------------||
+
+	'''
+	Maxwell's Equations
+	Gauss's law: Electric charges produce an electric field. ...
+	Gauss's law for magnetism: There are no magnetic monopoles. ...
+	Faraday's law: Time-varying magnetic fields produce an electric field.
+	Ampère's law: Steady currents and time-varying electric fields (the latter due to Maxwell's correction) produce a magnetic field.
+	'''
+
+	#Electricity
+
+	def power(voltage, current):
+		#P = V x I
+		P = voltage * current
+		return P
+
+	def power(voltage, current):
+		#V = I x R.    voltage = current x resistance
+		V = current * resistance
+		return V
+
+	#Q = I x t.     charge = current x time
+	#E = V x Q.     energy = voltage x charge
+	#E = V x I x t.     energy = voltage x current x time
+
+	#Transformer Equation
+	#Total cost  =  number of units x cost per unit.
 
 
-#----------------------- Constants -----------------------||
-c = 2.99792458 * (10 ** 8) # in m/s
-g = 9.81 # meters per second
 
-if __name__ = '__main__':
-	print('The speed of light:',c,'m/s')
-	print('Gravity: ',g,'m/s')
-
-#----------------------- Forces and Motion -----------------------||
-
-# Einstein's Thoery of Relativity
-def energy(m):
-	# energy = mass * speed of light squared
-	global c
-	# e = m * math.pow(c,2)
-	# e = m * (c ** 2)
-	e = m * (c * c)
-	return e
-
-def acceleration(v,u,t)
-	# acceleration = change in velocity ÷ time
-	a = (v-u) / t
-	return a
-
-# Newton's Second Law of Motion
-def force(m,a):
-	F = m * a
-	return F
-
-def speed(d,t):
-	# speed = distance ÷ time.
-	s = d / t
-	return s
-
-def weight(m):
-	# weight = mass x gravity
-	global g
-	w = m x g
-	return w
-
-     
-def density(m,v):
-	# density = mass ÷ volume 
-	d = m / v
-	return d
-
-
-'''
-p = m x v.     momentum = mass x velocity.
-(mv - mu) = F x t.     change in momentum = Force x time.
-p = F ÷ a.     pressure = force ÷ area.    
-m = F x d.     moment = force x perpendicular distance from pivot.
-'''
-
-
-#----------------------- Electro-Magnetism -----------------------||
-
-'''
-Maxwell's Equations
-Gauss's law: Electric charges produce an electric field. ...
-Gauss's law for magnetism: There are no magnetic monopoles. ...
-Faraday's law: Time-varying magnetic fields produce an electric field.
-Ampère's law: Steady currents and time-varying electric fields (the latter due to Maxwell's correction) produce a magnetic field.
-
-
-Electricity
-
-P = V x I.    power = voltage x current
-V = I x R.    voltage = current x resistance
-Q = I x t.     charge = current x time
-E = V x Q.     energy = voltage x charge
-E = V x I x t.     energy = voltage x current x time
-
-Transformer Equation
-Total cost  =  number of units x cost per unit.
-
-'''
-
-#----------------------- Energy -----------------------||
-'''
-Efficiency (%)  =  (useful energy out ÷ total energy in) x 100.
-GPE = mgh.      GPE = mass x gravity x height.      
-KE  =  ½mv2.     Kinetic Energy = 0·5 x mass x velocity2.
-W = F x d.      work done = force x distance.   
-W = E.      work done = energy transferred.
-P = E ÷ t.      power = energy ÷ time.            
-E = c x m x θ.   energy = specific heat capacity x mass x change in temperature.
-'''
+	#----------------------- Energy -----------------------||
+	'''
+	Efficiency (%)  =  (useful energy out ÷ total energy in) x 100.
+	GPE = mgh.      GPE = mass x gravity x height.
+	KE  =  ½mv2.     Kinetic Energy = 0·5 x mass x velocity2.
+	W = F x d.      work done = force x distance.   
+	W = E.      work done = energy transferred.
+	P = E ÷ t.      power = energy ÷ time.            
+	E = c x m x θ.   energy = specific heat capacity x mass x change in temperature.
+	'''
 
 
 
 
 
-#----------------------- Waves -----------------------||
-# v = f x λ.     velocity  =  frequency  x  wavelength
+	#----------------------- Waves -----------------------||
+	# v = f x λ.     velocity  =  frequency  x  wavelength
+
+
+	'''
+	engineering forces:
+		friction
+	 	leverage
+	'''
+
+	# l (string theory)
+
+	'''
+	Gravity
+	Electro-Magnetism
+		Electricity and magnetism
+	Weak Nuclear
+		Responsible for the radioactive decay of subatomic particles
+	Strong Nuclear
+		Binds the component particles of an atom's nucleus
+
+	maxwell's equations - the equations of light
+
+	antimatter
+		positron (anti-electron) has a positive charge
+		CERN has detected anti-helium 
+		when antimatter and matter combine, they create a huge force
+	'''
 
 
 
+	'''
+	particle zoo - the standard model
+	string theory is the bit below this
+	string field theory - michio kaku
+	sparticles: super particles
+	'''
 
-
-
-'''
-engineering forces:
-	friction
- 	leverage
-'''
-
-# l (string theory)
-
-'''
-Gravity
-Electro-Magnetism
-	Electricity and magnetism
-Weak Nuclear
-	Responsible for the radioactive decay of subatomic particles
-Strong Nuclear
-	Binds the component particles of an atom's nucleus
-
-maxwell's equations - the equations of light
-
-antimatter
-	positron (anti-electron) has a positive charge
-	CERN has detected anti-helium 
-	when antimatter and matter combine, they create a huge force
-
-
-
-'''
-
-'''
-particle zoo - the standard model
-
-
-string theory is the bit below this
-string field theory - michio kaku
-sparticles: super particles
-
-
-
-'''
+if __name__ == '__main__':
+	#print('The speed of light:',c,'m/s')
+	#print('Gravity: ',g,'m/s')
+	p = Physics()
+	print(p.c)
